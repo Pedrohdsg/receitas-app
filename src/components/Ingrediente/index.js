d3f2789d7afb06121400ea9bd8766b926e5c0535
Pedro Henrique Gondim <107958899+Pedrohdsg@users.noreply.github.com>
2023-04-20 18:35:02 -0300

    Update index.js

--- src/components/Ingrediente/index.js
+++ src/components/Ingrediente/index.js
@@ -1,22 +1,24 @@
-import styles from './Ingrediente.module.css'
+import styles from "./Ingrediente.module.css";
 
-function Ingrediente () {
-    return (
-        <>
-        <input type='text' className={styles.text}/>
-        <input type='number' className={styles.number}/>
+function Ingrediente() {
+  return (
+    <>
+      <div className={styles.ingredientes}>
+        <input type="text" className={styles.text} />
+        <input type="number"  min='0' className={styles.number} />
         <select className={styles.select}>
-            <option value="g">gramas</option>
-            <option value="xc">xícaras</option>
-            <option value="cs">colher sopa</option>
-            <option value="cc">colher chá</option>
-            <option value="un">unidades</option>
-            <option value="ml">mililitros(ml)</option>
-            <option value="a gosto">à gosto</option>
-            <option value="pitada">pitada</option>
+          <option value="g">gramas</option>
+          <option value="xc">xícaras</option>
+          <option value="cs">colher sopa</option>
+          <option value="cc">colher chá</option>
+          <option value="un">unidades</option>
+          <option value="ml">mililitros(ml)</option>
+          <option value="a gosto">à gosto</option>
+          <option value="pitada">pitada</option>
         </select>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Ingrediente;
